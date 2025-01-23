ed936d46703b3ca7deb9612af65cf5ccffb6d9d5
HugoFranceschi <franceschi.hugo@yahoo.com>
2025-01-23 13:11:38 +0100

    js

--- ex11.js
+++ ex11.js
@@ -1,7 +1,11 @@
 function premiereLettreMajuscule(chaine) {
-	chaine = chaine[0].toUpperCase() + chaine.slice(1);
-	chaine.split(" ");
-	return chaine;
+	let tableauMots = chaine.split(" ");
+
+	for (let i = 0; i < tableauMots.length; i++) {
+		tableauMots = tableauMots[0].toUpperCase() + chaine.slice(1);
+	}
+	return tableauMots;
+	// chaine = chaine[0].toUpperCase() + chaine.slice(1);
 }
 
 console.log(premiereLettreMajuscule("bonjour tout le monde")); // Résultat attendu : "Bonjour Tout Le Monde"
